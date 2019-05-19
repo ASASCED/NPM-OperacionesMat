239fc2f1517ea191e0f96d32f39ad410e3917b57
Anix <jesus_heaven117@outlook.com>
2019-05-19 00:29:50 -0500

    Primera version del modulo de matematicas basico

---
+++ index.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+    /**
+     * Suma de dos numeros
+     * @example
+     * n1 = 2, n2 = 5 => Resultado 7
+     * @param {*} n1 Numero uno de la suma 
+     * @param {*} n2 Numero dos de la suma
+     */
+    suma: function( n1, n2 ) {
+        return n1 + n2;
+    },
+    /**
+     * Resta de dos numeros
+     * @example
+     * n1 = 2, n2 = 5 => Resultado -3
+     * @param {*} n1 Numero uno de la resta 
+     * @param {*} n2 Numero dos de la resta
+     */
+    resta: function( n1, n2 ) {
+        return n1 - n2;
+    },
+    /**
+     * Multiplicacion de dos numeros
+     * @example
+     * n1 = 2, n2 = 5 => Resultado 10
+     * @param {*} n1 Numero uno de la multiplicacion 
+     * @param {*} n2 Numero dos de la multiplicacion
+     */
+    multiplicacion: function( n1, n2 ) {
+        return n1 * n2;
+    },
+    /**
+     * Division de dos numeros
+     * @example
+     * n1 = 2, n2 = 5 => Resultado 0.4
+     * @param {*} n1 Numero uno de la division 
+     * @param {*} n2 Numero dos de la division
+     */
+    division: function( n1, n2 ) {
+        return n1 / n2;
+    },
+
+}
---
+++ pruebas.js
@@ -0,0 +1,6 @@
+const m = require('.');
+
+console.log(m.suma(11, 2));
+console.log(m.resta(1, 24));
+console.log(m.multiplicacion(12, 2));
+console.log(m.division(100, 22));
